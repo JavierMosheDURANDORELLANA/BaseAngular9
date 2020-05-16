cb56c0b66cc88af518a9c1773e0bc291e4f12e35
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-16 18:13:13 -0500

    barrasuperior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{BarraSuperiorComponent} from './barra-superior/barra-superior.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BaseAngular9';
+  title = 'Next U - Aprendiendo Angular';
 }
