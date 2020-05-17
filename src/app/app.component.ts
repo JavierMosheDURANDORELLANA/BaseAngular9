eaf0e55f286300f1efc0bb8107d9bf71af45e223
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-17 15:24:58 -0500

    Hasta directiva * ngIF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BaseAngular9';
+  tituloMostrado = true;
+
+  buttonClicked(){
+    this.tituloMostrado = !this.tituloMostrado;
+  }
 }
