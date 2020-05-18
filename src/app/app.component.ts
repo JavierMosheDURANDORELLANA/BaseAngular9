b60a59c1646f59c474addb3d2dbcb1ffe8c6decc
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-18 17:14:08 -0500

    Culminación de curso pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BaseAngular9';
+  valor1 = 'hola, soy valor 1';
+  fecha1= new Date (2020, 0, 14);
+  numero1= 10;
+// 0 es enero
 }
